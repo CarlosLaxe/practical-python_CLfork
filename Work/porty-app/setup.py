fa3236862ed667b2f4bff33b292d1db280bc1761
carloslaxe <carlos.fuentes@gmx.com>
2025-10-24 15:28:31 +0100

    Exercises up to 9.5 (last one)

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Carlos Laxe",
+    author_email="notprovided@Python.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages()
+)
